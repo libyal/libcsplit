822f6cb06548d76d4500aee370c2ddbb10676b0d
Joachim Metz <joachim.metz@gmail.com>
2015-01-01 13:40:21 +0100

    Applied updates.

--- libcsplit/libcsplit_narrow_string.h
+++ libcsplit/libcsplit_narrow_string.h
@@ -1,7 +1,7 @@
 /*
  * Narrow character string functions
  *
- * Copyright (c) 2008-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
