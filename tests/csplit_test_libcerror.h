5d2aea6ccb29401a0c349c724e1e4feb55ae250e
Joachim Metz <joachim.metz@gmail.com>
2016-03-28 09:17:04 +0200

    Code clean up

--- tests/csplit_test_libcerror.h
+++ tests/csplit_test_libcerror.h
@@ -1,7 +1,7 @@
 /*
  * The internal libcerror header
  *
- * Copyright (C) 2012-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2016, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -44,7 +44,7 @@
 
 #include <libcerror.h>
 
-#endif
+#endif /* defined( HAVE_LOCAL_LIBCERROR ) */
 
-#endif
+#endif /* !defined( _CSPLIT_TEST_LIBCERROR_H ) */
 
