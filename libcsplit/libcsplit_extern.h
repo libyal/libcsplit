555b324fad5e44f84004739a95afe2ee79adbc23
Joachim Metz <joachim.metz@gmail.com>
2026-06-01 15:28:01 +0200

    Worked on tests and changes for CI tests

--- libcsplit/libcsplit_extern.h
+++ libcsplit/libcsplit_extern.h
@@ -24,6 +24,19 @@
 
 #include <common.h>
 
+#if !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute )
+#if __has_attribute( visibility )
+#define LIBCSPLIT_INTERNAL	__attribute__((visibility("hidden"))) extern
+
+#else
+#define LIBCSPLIT_INTERNAL	extern
+
+#endif /* __has_attribute( visibility ) */
+#else
+#define LIBCSPLIT_INTERNAL	extern
+
+#endif /* !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute ) */
+
 /* Define HAVE_LOCAL_LIBCSPLIT for local use of libcsplit
  */
 #if !defined( HAVE_LOCAL_LIBCSPLIT )
@@ -32,7 +45,7 @@
 
 #else
 #define LIBCSPLIT_EXTERN		/* extern */
-#define LIBCSPLIT_EXTERN_VARIABLE	extern
+#define LIBCSPLIT_EXTERN_VARIABLE	LIBCSPLIT_INTERNAL
 
 #endif /* !defined( HAVE_LOCAL_LIBCSPLIT ) */
 
